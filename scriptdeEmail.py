50ee99d5f782ae6162314312f3e36329bef53f58
Filipe <emaildofilipe.m@gmail.com>
2021-12-14 19:51:34 -0300

    Melhorias no scriptdeEmailDuplicado.

--- scriptdeEmail.py
+++ scriptdeEmail.py
@@ -26,36 +26,40 @@ def scriptEmailDuplicado():
         seTaxa = dadosduplicados['seTaxa'][i]
         taxa = dadosduplicados['taxa'][i]
         if seTaxa.lower() == 'sim':
-            #Imprimindo email
-            print("------------------------------------------------")
-            print("          ==Gerando Script de E-mail==          ")
-            print(f"         -- Número: {Identificador}/{len(dadosduplicados)}       ")
-            print("------------------------------------------------")
-            print(f"\n\n{nomeVendedor}, boa tarde!")
-            print(f"\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabela:")
-            #Procura de dados do tipoTabela....
-            dados = df_base.loc[linhaDuplicadas['cnpj'] == cnpj]
-            #Loop para 
-            for i in dados.index:
-                print("{} + {}".format(dados['tipoTabela'][i], dados['taxa'][i]))
-            print(f"Origem {origem}")
-            print("\nPor gentileza solicitar testes.")
-            print("\nAs taxas foram cadastradas no sistema devendo ser inserida manualmente na emissão.")
+            with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
+                #Imprimindo email
+                arquivo.write("\n------------------------------------------------")
+                arquivo.write("\n          ==Gerando Script de E-mail==          ")
+                arquivo.write(f"\n         -- Número: {Identificador}/{len(dadosduplicados)}       ")
+                arquivo.write("\n------------------------------------------------")
+                arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
+                arquivo.write(f"\n\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabela:")
+                #Procura de dados do tipoTabela....
+                dados = df_base.loc[linhaDuplicadas['cnpj'] == cnpj]
+                #Loop para 
+                for i in dados.index:
+                    arquivo.write("\n{} + {}".format(dados['tipoTabela'][i], dados['taxa'][i]))
+                arquivo.write(f"\nOrigem {origem}")
+                arquivo.write("\n\nPor gentileza solicitar testes.")
+                arquivo.write("\n\nAs taxas foram cadastradas no sistema devendo ser inserida manualmente na emissão.")
         elif seTaxa.lower() == 'não':
-             #Imprimindo email
-            print("------------------------------------------------")
-            print("          ==Gerando Script de E-mail==          ")
-            print(f"         -- Número: {Identificador}/{len(dadosduplicados)}       ")
-            print("------------------------------------------------")
-            print(f"\n\n{nomeVendedor}, boa tarde!")
-            print(f"\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabela:")
-            #Procura de dados do tipoTabela....
-            dados = df_base.loc[linhaDuplicadas['cnpj'] == cnpj]
-            #Loop para 
-            for i in dados.index:
-                print(dados['tipoTabela'][i])
-            print(f"Origem {origem}")
-            print("\nPor gentileza solicitar testes.")
+            with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
+                #Imprimindo email
+                arquivo.write("\n------------------------------------------------")
+                arquivo.write("\n          ==Gerando Script de E-mail==          ")
+                arquivo.write(f"\n         -- Número: {Identificador}/{len(dadosduplicados)}       ")
+                arquivo.write("\n------------------------------------------------")
+                arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
+                arquivo.write(f"\n\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabela:")
+                #Procura de dados do tipoTabela....
+                dados = df_base.loc[linhaDuplicadas['cnpj'] == cnpj]
+                #Loop para 
+                for i in dados.index:
+                    arquivo.write("\n{}".format(dados['tipoTabela'][i]))
+                arquivo.write(f"Origem {origem}")
+                arquivo.write("\n\nPor gentileza solicitar testes.")
+        else:
+            print("Não foi possível gerar o email.")
 
 def scriptEmailUnico():
     #Definindo critérios
@@ -99,4 +103,8 @@ def scriptEmailUnico():
                 arquivo.write("\n\nAs taxas foram cadastradas no sistema devendo ser inserida manualmente na emissão.")
         else:
             print("Não foi possível gerar os scripts, informar o desenvolvedor.")
+
+print("\nGerando Script de E-mail....")
 scriptEmailDuplicado()
+scriptEmailUnico()
+print("Email - Aplicações Finalizadas!")
