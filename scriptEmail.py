306f4b6d45e12c638dd66428fae91a33296e8630
Filipe <emaildofilipe.m@gmail.com>
2021-12-15 19:06:56 -0300

    Atualizando scriptEmail.

--- scriptEmail.py
+++ scriptEmail.py
@@ -5,21 +5,21 @@ df_base = pd.read_excel('base.xlsx',dtype=object)
 
 lista_vendedores = list(set(df_base['nomeVendedor'].values.tolist()))
 
-df_temp = pd.DataFrame()
+base_temporaria = pd.DataFrame()
 
 def scriptEmail():
     for vendedor in lista_vendedores:
-        df_temp = df_base[df_base['nomeVendedor'] == vendedor]
-        lista_cnpj = list(set(df_temp['cnpj'].values.tolist()))
+        base_temporaria = df_base[df_base['nomeVendedor'] == vendedor]
+        lista_cnpj = list(set(base_temporaria['cnpj'].values.tolist()))
         for cnpj in lista_cnpj:
-            df_cnpj = df_temp[df_temp['cnpj'] == cnpj]
+            base_por_cnpj = base_temporaria[base_temporaria['cnpj'] == cnpj]
             nomeVendedor = vendedor
             cnpj = cnpj
-            for i in df_cnpj.index:
-                origem = df_cnpj['Origem'][i]
-                seTaxa = df_cnpj['seTaxa'][i]
-                taxa = df_cnpj['taxa'][i]
-            if seTaxa.lower() == 'sim' and len(df_cnpj['tipoTabela']) > 1:
+            for i in base_por_cnpj.index:
+                origem = base_por_cnpj['Origem'][i]
+                seTaxa = base_por_cnpj['seTaxa'][i]
+                taxa = base_por_cnpj['taxa'][i]
+            if seTaxa.lower() == 'sim' and len(base_por_cnpj['tipoTabela']) > 1:
                 with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
                     #Imprimindo email
                     arquivo.write("\n------------------------------------------------")
@@ -28,12 +28,12 @@ def scriptEmail():
                     arquivo.write("\n------------------------------------------------")
                     arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
                     arquivo.write(f"\n\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabelas:")
-                    for i in df_cnpj.index:
-                        arquivo.write("\n{} + {}".format(df_cnpj['tipoTabela'][i],df_cnpj['taxa'][i]))
+                    for i in base_por_cnpj.index:
+                        arquivo.write("\n{} + {}".format(base_por_cnpj['tipoTabela'][i],base_por_cnpj['taxa'][i]))
                     arquivo.write(f"\nOrigem {origem}")
                     arquivo.write("\n\nPor gentileza solicitar testes.")
                     arquivo.write("\n\nAs taxas foram cadastradas no sistema devendo ser inserida manualmente na emissão.")
-            elif seTaxa.lower() == 'sim' and len(df_cnpj['tipoTabela']) == 1:
+            elif seTaxa.lower() == 'sim' and len(base_por_cnpj['tipoTabela']) == 1:
                 with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
                     #Imprimindo email
                     arquivo.write("\n------------------------------------------------")
@@ -42,12 +42,12 @@ def scriptEmail():
                     arquivo.write("\n------------------------------------------------")
                     arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
                     arquivo.write(f"\n\nConforme solicitado foi cadastrada no CNPJ {cnpj} a tabela:")
-                    for i in df_cnpj.index:
-                        arquivo.write("\n{} + {}".format(df_cnpj['tipoTabela'][i],df_cnpj['taxa'][i]))
+                    for i in base_por_cnpj.index:
+                        arquivo.write("\n{} + {}".format(base_por_cnpj['tipoTabela'][i],base_por_cnpj['taxa'][i]))
                     arquivo.write(f"\nOrigem {origem}")
                     arquivo.write("\n\nPor gentileza solicitar testes.")
                     arquivo.write("\n\nA taxa foi cadastrada no sistema devendo ser inserida manualmente na emissão.")
-            elif seTaxa.lower() == 'não' and len(df_cnpj['tipoTabela']) > 1:
+            elif seTaxa.lower() == 'não' and len(base_por_cnpj['tipoTabela']) > 1:
                 with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
                     #Imprimindo email
                     arquivo.write("\n------------------------------------------------")
@@ -56,11 +56,11 @@ def scriptEmail():
                     arquivo.write("\n------------------------------------------------")
                     arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
                     arquivo.write(f"\n\nConforme solicitado foram cadastradas no CNPJ {cnpj} as tabela:")
-                    for i in df_cnpj.index:
-                        arquivo.write("\n{}".format(df_cnpj['tipoTabela'][i]))
+                    for i in base_por_cnpj.index:
+                        arquivo.write("\n{}".format(base_por_cnpj['tipoTabela'][i]))
                     arquivo.write(f"\nOrigem {origem}")
                     arquivo.write("\n\nPor gentileza solicitar testes.")
-            elif seTaxa.lower() == 'não' and len(df_cnpj['tipoTabela']) == 1:
+            elif seTaxa.lower() == 'não' and len(base_por_cnpj['tipoTabela']) == 1:
                 with open(f'scriptEmail\{cnpj}.txt', 'w') as arquivo:
                     #Imprimindo email
                     arquivo.write("\n------------------------------------------------")
@@ -69,21 +69,21 @@ def scriptEmail():
                     arquivo.write("\n------------------------------------------------")
                     arquivo.write(f"\n\n{nomeVendedor}, boa tarde!")
                     arquivo.write(f"\n\nConforme solicitado foi cadastrada no CNPJ {cnpj} a tabela:")
-                    for i in df_cnpj.index:
-                        arquivo.write("\n{}".format(df_cnpj['tipoTabela'][i]))
+                    for i in base_por_cnpj.index:
+                        arquivo.write("\n{}".format(base_por_cnpj['tipoTabela'][i]))
                     arquivo.write(f"\nOrigem {origem}")
                     arquivo.write("\n\nPor gentileza solicitar testes.")
             else:
                 print("Não foi possível gerar o email.")
-print("\n-----------------------------")
-print("Gerando Script de E-mail....")
-scriptEmail()
-time.sleep(1)
-print("\nEmails Únicos - OK")
-time.sleep(1)
-print("Emails Duplos - OK")
-time.sleep(1)
-print("\n-----------------------------")
-print("Aplicações Finalizadas!")
-
-
+def gerandoEmail():
+    print("\n-----------------------------")
+    print("Gerando Script de E-mail....")
+    scriptEmail()
+    time.sleep(1)
+    print("\nEmails Únicos - OK")
+    time.sleep(1)
+    print("Emails Duplos - OK")
+    time.sleep(1)
+    print("\n-----------------------------")
+    print("Aplicações Finalizadas!")
+gerandoEmail()
